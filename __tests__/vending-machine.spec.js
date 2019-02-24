561fa805368435590fbd99956835cacacbe42a83
Timmy <timgabrielnguyen@gmail.com>
2019-02-23 16:44:52 -0800

    Refactored Machine Props

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -1,54 +1,20 @@
-const VendingMachine = require("../src/vending-machine");
+const VendingMachine = require("../src/vending-machine.js");
 
-describe("vending machine", () => {
-  let test = {};
-
+describe("State of Vending Machine", () => {
+  let MachineConstructor;
   beforeEach(() => {
-    test.inventory = {
-      1: {
-        productId: 1,
-        productName: "Honey Butter Chips",
-        productQuantity: 10,
-        productPrice: 3.0
-      },
-      2: {
-        productId: 2,
-        productName: "Ruts Candy",
-        productQuantity: 10,
-        productPrice: 2.85
-      },
-      3: {
-        productId: 3,
-        productName: "Sugarpova",
-        productQuantity: 10,
-        productPrice: 3.6
-      },
-      4: {
-        productId: 4,
-        productName: "Nerds",
-        productQuantity: 10,
-        productPrice: 4.1
-      },
-      5: {
-        productId: 5,
-        productName: "Hersheys",
-        productQuantity: 10,
-        productPrice: 5.0
-      }
-    };
-    test.vendingMachine = new VendingMachine(test.inventory);
+    MachineConstructor = new VendingMachine("../selections.json");
   });
 
-  describe('.printInventory', () => {
-    beforeEach(() => {
-      test.vendingMachine = new VendingMachine(test.inventory);
+
+describe("Products Properties", () => {
+  it("Machine to return name and quantity of selected items", () => {
+    expect(MachineConstructor.getProduct("Honey Butter Chips")).toEqual({
+      name: "Honey Butter Chips",
+      quantity: 10
     });
-
-    it('the machine should print out the test inventory', () => {
-      const result = test.vendingMachine.printInventory()
-      expect(result).toEqual(true)
-    })
   });
-
-  
 });
+
+});
+
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,29 +1,20 @@
-const MachineStock = require("./machine-stock");
-
 class VendingMachine {
-  constructor(initialStock) {
-    this.numberOfSlots = 5;
+  constructor(json) {
+    this.data = require(json);
+  }
 
-    this.coinsStock = {
-      toonies: { quantity: 100, value: 2.0 },
-      loonies: { quantity: 100, value: 1.0 },
-      quarters: { quantity: 100, value: 0.25 },
-      dimes: { quantity: 100, value: 0.1 },
-      nickels: { quantity: 100, value: 0.05 }
+  getProduct(product) {
+    return {
+      name: this.data.selections[product].name,
+      quantity: this.data.selections[product].quantity
     };
-    // this.coinsStockKeys = Object.keys(this.coinsStock);
-
-    this.inventory = MachineStock(initialStock, this.numberOfSlots);
-    // this.inventoryKeys = Object.keys(this.inventory);
   }
 
-  printInventory() {
-    //this method is to console log the output for each item in the inventory
-    // this.inventoryKeys.forEach( key => {
-    console.log(this.inventory);
-    // });
-    return true;
-  }
+
+
 }
 
 module.exports = VendingMachine;
+
+
+
