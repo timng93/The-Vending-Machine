12155f0841862a4f3bb961c2ff2f035c9c3fc154
Timmy <timgabrielnguyen@gmail.com>
2019-02-23 18:38:59 -0800

    Dispensed Item and Returned Change

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -9,12 +9,31 @@ describe("State of Vending Machine", () => {
 
 describe("Products Properties", () => {
   it("Machine to return name and quantity of selected items", () => {
-    expect(MachineConstructor.getProduct("Honey Butter Chips")).toEqual({
+    expect(MachineConstructor.getProductCount("Honey Butter Chips")).toEqual({
       name: "Honey Butter Chips",
       quantity: 10
     });
   });
+  it("Machine to return name and price of selected items", () => {
+    expect(MachineConstructor.getProductPrice("Honey Butter Chips")).toEqual({
+      name: "Honey Butter Chips",
+      price: 3.0
+  })
 });
+});
+
+describe("Payment", () => {
+  it("should return item as payment is equal to price", () => {
+    expect(MachineConstructor.getPayment("Honey Butter Chips", 3.00)).toEqual({
+      name: "Honey Butter Chips", change: 0, quantity: 9
+    })
+  })
+  it("should return change as payment is more than price", () => {
+  expect(MachineConstructor.getPayment("Honey Butter Chips", 4.00)).toEqual({
+    name: "Honey Butter Chips", change: 1.0, quantity: 9
+  })
+});
+})
 
 });
 
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -3,18 +3,38 @@ class VendingMachine {
     this.data = require(json);
   }
 
-  getProduct(product) {
+  getProductCount(product) {
     return {
       name: this.data.selections[product].name,
       quantity: this.data.selections[product].quantity
     };
   }
 
+  getProductPrice(product) {
+    return {
+      name: this.data.selections[product].name,
+      price: this.data.selections[product].price
+    };
+  }
 
+  getPayment(product, payment) {
+    let selectedItem = this.data.selections[product].name;
+    let selectedItemPrice = this.data.selections[product].price;
+    let selectedItemQuantity = this.data.selections[product].quantity;
 
-}
+    let transaction={
+        name : "",
+        change : "",
+        quantity : ""
+    }
+
+    if(payment === selectedItemPrice || payment > selectedItemPrice) {
+      transaction.change = payment - selectedItemPrice;
+      transaction.name = selectedItem;
+      transaction.quantity = selectedItemQuantity -= 1;
+    }
+    return transaction 
+  }
+};
 
 module.exports = VendingMachine;
-
-
-
